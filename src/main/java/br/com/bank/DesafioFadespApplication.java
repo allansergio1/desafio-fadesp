2a5f7011209ea67e90daa49d8d80a919dd34050d
Allan Sérgio <allansergio1@gmail.com>
2025-03-22 00:52:26 -0300

    initial commit

---
+++ src/main/java/br/com/bank/DesafioFadespApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFadespApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioFadespApplication.class, args);
+    }
+
+}
